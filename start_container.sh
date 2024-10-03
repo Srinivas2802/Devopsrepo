02b23c3356405121b719b68aa8b8818fc399c035
Srinivas2802 <91659129+Srinivas2802@users.noreply.github.com>
2024-10-04 00:13:46 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-set -e
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Check if the script is being run as root
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run as root"
+  exit 1
+else
+  echo "Running as root user"
+fi
 
 # Pull the Docker image from Docker Hub
+echo "Pulling Docker image srinivasv2802/codedeploy:latest"
 docker pull srinivasv2802/codedeploy:latest
 
+# Check if the Docker pull was successful
+if [ $? -ne 0 ]; then
+  echo "Failed to pull the Docker image."
+  exit 1
+fi
+
 # Run the Docker image as a container
+echo "Running Docker container from image srinivasv2802/codedeploy:latest"
 docker run -d -p 5000:5000 srinivasv2802/codedeploy:latest
+
+# Check if the Docker container started successfully
+if [ $? -eq 0 ]; then
+  echo "Docker container started successfully."
+else
+  echo "Failed to start Docker container."
+  exit 1
+fi
+
