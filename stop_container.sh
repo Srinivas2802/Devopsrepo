71058e700cc97be6f484a9ac1047056d253c749d
Srinivas2802 <91659129+Srinivas2802@users.noreply.github.com>
2024-10-04 00:46:04 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# Check if the script is being run as root
-if [ "$EUID" -ne 0 ]; then 
-  echo "Please run as root"
-  exit 1
-else
-  echo "Running as root user"
-fi
-
-# Get the container ID of the running container (assuming the container is using the image srinivasv2802/codedeploy)
 CONTAINER_ID=$(docker ps -q --filter ancestor=srinivasv2802/codedeploy:latest)
 
 # Check if any containers are running for the image
@@ -19,11 +10,3 @@ else
   echo "Stopping container with ID: $CONTAINER_ID"
   docker stop "$CONTAINER_ID"
   
-  # Verify if the container stopped successfully
-  if [ $? -eq 0 ]; then
-    echo "Container stopped successfully."
-  else
-    echo "Failed to stop the container."
-    exit 1
-  fi
-fi
